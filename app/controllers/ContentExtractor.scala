910193dea95214bce7ebcd4f122874e9c9891518
Taras <taras.baranyuk@gmail.com>
2013-01-06 02:24:15 +0400

    jar with memmory check

--- app/controllers/ContentExtractor.scala
+++ app/controllers/ContentExtractor.scala
@@ -55,6 +55,21 @@ class ContentExtractor (val site: Site) {
   private var cache: Elements = new Elements()
   private var lastUpdateTime:Long = 0
 
+  /*
+  def addLinks (elemsin:Elements):Elements = {
+    if (elemsin.size() > 0) correctrss = true
+    else correctrss = false
+    val i = elemsin.iterator()
+    val elemsout = new Elements()
+    while(i.hasNext) {
+      val e = i.next()
+      elemsout.add(e.select("description").get(0).attr("site", e.select("link").text())
+        .tagName("div").addClass("well"))
+    }
+    elemsout
+  }
+  */
+
   def getContent: Elements = {
     if (System.currentTimeMillis() - lastUpdateTime < 60000) cache
     else {
@@ -73,140 +88,3 @@ class ContentExtractor (val site: Site) {
     }
   }
 }
-
-/*
-class ContentExtractor2 (val site: Site) {
-
-  val connection = Jsoup.connect(site.url).ignoreContentType(true).ignoreHttpErrors(true).timeout(60000)
-
-  var html:Document = {
-    if (connection != null)
-      try {
-        connection.get()
-      }
-      catch {
-        case _:IOException | _:MalformedURLException | _:HttpStatusException |
-             _:UnsupportedMimeTypeException | _:SocketTimeoutException |
-             _:java.net.UnknownHostException => Document.createShell("")
-      }
-    else
-      Document.createShell("")}
-  var content = html.select(site.parsel)
-  var rss_links = html.select("link[type=application/rss+xml]")
-  var correctrss = false
-/*
-  def getRSSConnection = {
-    try {
-      if (rss_links.size() > 0 && rss_links.get(0).attr("href").contains(site.url)) {
-        Jsoup.connect(rss_links.get(0).attr("href")).ignoreContentType(true).ignoreHttpErrors(true).
-          timeout(60000)
-      }
-      else null
-    } catch {
-        case _: IllegalArgumentException | _: NullPointerException | _: UnknownHostException => null
-    }
-
-  }
-
-  var rssconnection = getRSSConnection
-
-  var rss:Document = {
-    if (rssconnection != null)
-      try {
-        rssconnection.get()
-      }
-      catch {
-        case _:IOException | _:MalformedURLException | _:HttpStatusException |
-             _:UnsupportedMimeTypeException | _:SocketTimeoutException |
-             _:java.net.UnknownHostException => Document.createShell("")
-      }
-    else
-      Document.createShell("")}
-
-  */
-
-  def addLinks (elemsin:Elements):Elements = {
-    if (elemsin.size() > 0) correctrss = true
-    else correctrss = false
-    val i = elemsin.iterator()
-    val elemsout = new Elements()
-    while(i.hasNext) {
-      val e = i.next()
-      elemsout.add(e.select("description").get(0).attr("site", e.select("link").text())
-        .tagName("div").addClass("well"))
-    }
-    elemsout
-  }
-  def getRSSContent():Elements = {
-    if (rss_links.size() > 0 && rss_links.get(0).attr("href").contains(site.url)) {
-//    if (rssconnection != null) {
-      try {
-/*        val url = new URL(rss.get(0).attr("href"))
-        val stream = url.openStream()
-        val rssParser = Jsoup.parse(stream, site.encoding, rss.get(0).attr("href"), Parser.xmlParser())
-*/
-        val rssconnection = Jsoup.connect(rss_links.get(0).attr("href")).ignoreContentType(true).ignoreHttpErrors(true).
-          timeout(60000)
-        val rss = rssconnection.get()
-        val rssParser = Jsoup.parse(rss.html(), rss_links.get(0).attr("href"), Parser.xmlParser())
-//        stream.close()
-        addLinks(rssParser.select("item"))
-      } catch {
-        case _: IllegalArgumentException | _: NullPointerException |
-             _: ArrayIndexOutOfBoundsException | _: UnknownHostException => getContent()
-      }
-    }
-    else getContent()
-  }
-/*
-  def update () {
-    if (correctrss == false) {
-      html = {
-        if (connection != null)
-          try {
-            connection.get()
-          }
-          catch {
-            case _:IOException | _:MalformedURLException | _:HttpStatusException |
-                 _:UnsupportedMimeTypeException | _:SocketTimeoutException |
-                 _:java.net.UnknownHostException => Document.createShell("")
-          }
-        else
-          Document.createShell("")}
-      content = html.select(site.parsel)
-    }
-    else
-    {
-      rss = {
-      if (rssconnection != null)
-        try {
-          rssconnection.get()
-        }
-        catch {
-          case _:IOException | _:MalformedURLException | _:HttpStatusException |
-               _:UnsupportedMimeTypeException | _:SocketTimeoutException |
-               _:java.net.UnknownHostException => Document.createShell("")
-        }
-      else
-        Document.createShell("")}
-    }
-  }
-*/
-  def update () {
-    html = connection.get()
-    content = html.select(site.parsel)
-  }
-
-  def getContent (id: Int = -1, count: Int = 1): Elements = {
-    correctrss = false
-    var elems = content
-    if (id != -1 && id < content.size()) {
-      elems = new Elements()
-      for (i <- id to id + count)
-        elems.add(content.get(i))
-    }
-    //нада тоже доделать формирование полного адреса ссылки
-    elems.attr("site", site.url).tagName("div").addClass("well")
-  }
-}
-  */
