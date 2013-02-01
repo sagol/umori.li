266f761f96a527605e84eb52016f417ef752b2a2
Taras <taras.baranyuk@gmail.com>
2013-02-01 23:23:40 +0400

    added tweet and facebook share links

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -30,10 +30,23 @@ object Application extends Controller {
     val content = SourceInstances.urlContent(url.getOrElse(""))
     val meta = "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n\t\t" +
                "<meta name=\"title\" content=\"" +
-                  StringEscapeUtils.escapeHtml4(content.head.element.text().take(20)) + "\" />\n\t\t" +
+                  StringEscapeUtils.escapeHtml4(content.head.element.text().take(30)) + "...\" />\n\t\t" +
                "<meta name=\"description\" content=\"" +
                   StringEscapeUtils.escapeHtml4(content.head.element.text().take(120)) + "\" />\n\t\t" +
-               "<link rel=\"image_src\" href=\"http://www.umori.li/assets/images/big_smile72.png\" />"
+               "<link rel=\"image_src\" href=\"http://www.umori.li/assets/images/big_smile72.png\" />\n\t\t" +
+               "<meta property=\"og:title\" content=\"" +
+                  StringEscapeUtils.escapeHtml4(content.head.element.text().take(30)) + "...\" />\n\t\t" +
+               "<meta property=\"og:description\" content=\"" +
+                  StringEscapeUtils.escapeHtml4(content.head.element.text().take(120)) + "\" />\n\t\t" +
+               "<meta property=\"og:image\" href=\"http://www.umori.li/assets/images/big_smile72.png\" />\n\t\t" +
+//               "<meta name=\"twitter:title\" content=\"" +
+  //                StringEscapeUtils.escapeHtml4(content.head.element.text().take(65)) + "...\" />\n\t\t" +
+    //           "<meta name=\"twitter:description\" content=\"" +
+      //            StringEscapeUtils.escapeHtml4(content.head.element.text().take(199)) + "\" />\n\t\t" +
+        //       "<meta name=\"twitter:image\" href=\"http://www.umori.li/assets/images/big_smile72.png\" />" +
+               "<meta name=\"twitter:card\" content=\"summary\" />"
+
+
     Ok(views.html.url("...", content)(meta))
   }
 
